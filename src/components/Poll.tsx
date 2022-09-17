cbd509fee53aae8777c6647d4c7d43ab06ae3660
ngTurk <malik_alturk@hotmail.com>
2022-09-17 23:26:21 +0300

    implement poll component ui

--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -1,3 +1,38 @@
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+
+// Icons
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
+
+// Styled components
+const OptionsTitle = styled(Typography)(({ theme }) => ({
+    margin: '24px 0 0',
+    fontWeight: 500
+}));
+
 export default function Poll(): JSX.Element {
-  return <div>Poll</div>;
+  return (
+    <Box flexDirection="column" padding="12px" marginTop={5}>
+        <TextField label="Question" variant="outlined" fullWidth />
+        <OptionsTitle variant="body1">
+            Answers:
+        </OptionsTitle>
+        <Box display="flex" marginTop={1}>
+            <TextField placeholder="Type an answer" variant="outlined" fullWidth />
+            <Button sx={{ marginLeft: 1 }} variant="outlined">
+                <CloseIcon />
+            </Button>
+        </Box>
+        <Box display="flex" marginTop={1}>
+            <TextField placeholder="Type an answer" variant="outlined" fullWidth />
+            <Button sx={{ marginLeft: 1 }} variant="outlined">
+                <AddIcon />
+            </Button>
+        </Box>
+    </Box>
+  );
 }
